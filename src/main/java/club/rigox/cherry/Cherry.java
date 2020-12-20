0e295018b92aa2e33004ad59c5ffa5d7dae2c46f
sebastnchan <vauntex18@gmail.com>
2020-12-20 20:29:10 -0300

    MongoDB added + Removed deprecation methods.

--- src/main/java/club/rigox/cherry/Cherry.java
+++ src/main/java/club/rigox/cherry/Cherry.java
@@ -1,14 +1,61 @@
 package club.rigox.cherry;
 
+import club.rigox.cherry.database.MongoDB;
+import club.rigox.cherry.utils.Config;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import static club.rigox.cherry.utils.Logger.error;
+import static club.rigox.cherry.utils.Logger.info;
+
 public final class Cherry extends JavaPlugin {
-    private Cherry instance;
+    public static Cherry instance;
+
+    private MongoDB mongo;
+    private Config config;
+    private FileConfiguration database;
 
     @Override
     public void onEnable() {
         instance = this;
 
+        mongo = new MongoDB(this);
+
+        registerHooks();
+        loadConfigs();
+        mongo.connect();
     }
 
+    @Override
+    public void onDisable() {
+        mongo.close();
+    }
+
+    public void registerHooks() {
+        if (getServer().getPluginManager().getPlugin("PlaceholderAPI") == null) {
+            error("Could not find PlaceholderAPI, disabling plugin...");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        info("Hooked with PlaceholderAPI successfully!");
+
+        if (getServer().getPluginManager().getPlugin("ScoreboardAPI") == null) {
+            error("Could not find ScoreboardAPI, disabling plugin...");
+            return;
+        }
+        info("Hooked with ScoreboardAPI successfully!");
+    }
+
+    public void loadConfigs() {
+        config = new Config(this);
+        this.database = config.createConfig("database");
+    }
+
+    public FileConfiguration getDatabase() {
+        return database;
+    }
+
+    public MongoDB getMongo() {
+        return mongo;
+    }
 }
---
+++ src/main/java/club/rigox/cherry/database/MongoDB.java
@@ -0,0 +1,36 @@
+package club.rigox.cherry.database;
+
+import club.rigox.cherry.Cherry;
+import com.mongodb.*;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bukkit.Bukkit;
+
+import static club.rigox.cherry.utils.Logger.*;
+
+public class MongoDB {
+    private final Cherry cherry;
+    private MongoCollection playerCollection;
+    private MongoClient client;
+
+    public MongoDB (Cherry plugin) {
+        this.cherry = plugin;
+    }
+
+    public void connect(){
+        MongoClientURI uri = new MongoClientURI(String.format("mongodb+srv://%s:%s@%s/admin?retryWrites=true&w=majority",
+                cherry.getDatabase().getString("MONGO.AUTH.USERNAME"),
+                cherry.getDatabase().getString("MONGO.AUTH.PASSWORD"),
+                cherry.getDatabase().getString("MONGO.HOST")));
+
+        client = new MongoClient(uri);
+
+        MongoDatabase database = client.getDatabase(cherry.getDatabase().getString("MONGO.DATABASE"));
+        playerCollection = database.getCollection("players");
+    }
+
+    public void close() {
+        client.close();
+        info("MongoDB connection has been closed!");
+    }
+}
