741f11559262a668d194db93c82d92231a1a9595
sebastnchan <vauntex18@gmail.com>
2020-12-22 22:09:52 -0300

    Unnecessary debug...

--- src/main/java/club/rigox/cherry/database/MongoDB.java
+++ src/main/java/club/rigox/cherry/database/MongoDB.java
@@ -67,7 +67,6 @@ public class MongoDB {
         BasicDBObject set = new BasicDBObject("$set", r);
         set.append("$set", new BasicDBObject("credits", credits));
         playerCollection.update(found, set);
-        debug("Player left. Saving their cool hard worked credits.");
     }
 
 }
