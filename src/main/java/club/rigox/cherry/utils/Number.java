14ce3dab8376e8cae02ee7b526ad79f8041d1753
sebastnchan <vauntex18@gmail.com>
2020-12-28 13:03:18 -0300

    Decimal format :)

--- src/main/java/club/rigox/cherry/utils/Number.java
+++ src/main/java/club/rigox/cherry/utils/Number.java
@@ -3,11 +3,14 @@ package club.rigox.cherry.utils;
 import org.apache.commons.lang.math.NumberUtils;
 import org.bukkit.command.CommandSender;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.Map;
 import java.util.NavigableMap;
 import java.util.TreeMap;
 
 import static club.rigox.cherry.utils.Config.getLangString;
+import static club.rigox.cherry.utils.Logger.debug;
 import static club.rigox.cherry.utils.Logger.sendMessage;
 
 public class Number {
@@ -23,24 +26,25 @@ public class Number {
     static {
         suffixes.put(1_000d, "k");
         suffixes.put(1_000_000d, "M");
-        suffixes.put(1_000_000_000d, "G");
+        suffixes.put(1_000_000_000d, "B");
         suffixes.put(1_000_000_000_000d, "T");
-        suffixes.put(1_000_000_000_000_000d, "P");
-        suffixes.put(1_000_000_000_000_000_000d, "E");
+        suffixes.put(1_000_000_000_000_000d, "Q");
+        suffixes.put(1_000_000_000_000_000_000d, "Q2");
     }
 
     public static String format(double value) {
-        //Long.MIN_VALUE == -Long.MIN_VALUE so we need an adjustment here
-        if (value == Long.MIN_VALUE) return format(Long.MIN_VALUE + 1);
+        if (value == Double.MIN_VALUE) return format(Double.MIN_VALUE + 1);
         if (value < 0) return "-" + format(-value);
-        if (value < 1000) return Double.toString(value); //deal with easy case
+        if (value < 1000) return Double.toString(value);
 
         Map.Entry<Double, String> e = suffixes.floorEntry(value);
         Double divideBy = e.getKey();
         String suffix = e.getValue();
 
-        double truncated = value / (divideBy / 10); //the number part of the output times 10
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        double truncated = value / (divideBy / 10);
         boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
-        return hasDecimal ? (truncated / 10d) + suffix : (truncated / 10) + suffix;
+        return hasDecimal ? df.format((truncated / 10d)) + suffix : df.format((truncated / 10)) + suffix;
     }
 }
